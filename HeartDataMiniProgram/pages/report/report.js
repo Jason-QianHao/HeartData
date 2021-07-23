c75c848f78cdb387a0e0f87f6de0011838619c28
Jason-QianHao <qh404836248@163.com>
2021-07-23 20:43:47 +0800

    新增心电报告页面框架

--- HeartDataMiniProgram/pages/report/report.js
+++ HeartDataMiniProgram/pages/report/report.js
@@ -5,62 +5,97 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    reports: {}
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    var that = this;
+    // 解析JSON对象
+    /*
+    {
+      "2020": {
+        "1": {
+          "imgurl": "",
+          "title": "",
+          "description": "",
+          "analysis": {
+            "HealthIndex": "",
+            "Other": ""
+          },
+          "dayLists":{
+            "1": {
+              "isUsed": "",
+              "HealthIndex" : ""
+            }
+          }
+        }
+      },
+      "2021": ...
+    }
+    */
+    var res = '{ "data" : ['+
+                '{ '+
+                  '"year": "2020",'+
+                  '"month": ['+
+                    '{ "m": "1",' +
+                      '"imgurl": "/static/imgs/reports/riqi1.png",'+
+                      '"title": "1月报告",'+
+                      '"description": "1月报告，良好",'+
+                      '"analysis": {'+
+                        '"HealthIndex": "",'+
+                        '"Other": ""'+
+                      '},'+
+                      '"dayLists":['+
+                        '{ "d" : "31",'+
+                          '"isUsed": "",'+
+                          '"HealthIndex" : ""'+
+                        '}'+
+                      ']'+
+                    '},'+
+                    '{ "m": "2",' +
+                      '"imgurl": "/static/imgs/reports/riqi2.png",'+
+                      '"title": "2月报告",'+
+                      '"description": "2月报告，良好",'+
+                      '"analysis": {'+
+                        '"HealthIndex": "",'+
+                        '"Other": ""'+
+                      '},'+
+                      '"dayLists":['+
+                        '{ "d" : "28",'+
+                          '"isUsed": "",'+
+                          '"HealthIndex" : ""'+
+                        '}'+
+                      ']'+
+                    '}'+
+                  ']'+
+                '},'+
+                '{ '+
+                  '"year": "2021",'+
+                  '"month": ['+
+                    '{ "m": "2",' +
+                      '"imgurl": "/static/imgs/reports/riqi2.png",'+
+                      '"title": "2月报告",'+
+                      '"description": "2月报告，良好",'+
+                      '"analysis": {'+
+                        '"HealthIndex": "",'+
+                        '"Other": ""'+
+                      '},'+
+                      '"dayLists":['+
+                        '{ "d" : "31",'+
+                          '"isUsed": "",'+
+                          '"HealthIndex" : ""'+
+                          '}'+
+                        ']'+
+                      '}'+
+                    ']'+
+                '}'+               
+              ']}';
+    var obj = JSON.parse(res);
+    console.log(obj);
+    that.setData({
+      reports: obj
+    });
   }
 })
